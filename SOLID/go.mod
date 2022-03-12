8cc88215781cd589f51926e735b219acbf03e98d
Samuel Pereira Machado <samuel.pereira@faci.ly>
2022-03-12 12:40:22 -0300

    SOLID: S && O

---
+++ SOLID/go.mod
@@ -0,0 +1,3 @@
+module github.com/samuelmachado/go-design-patterns/SOLID
+
+go 1.16
---
+++ SOLID/main.go
@@ -0,0 +1,7 @@
+package main
+
+import srp "github.com/samuelmachado/go-design-patterns/SOLID/SRP"
+
+func main() {
+	srp.Do()
+}
